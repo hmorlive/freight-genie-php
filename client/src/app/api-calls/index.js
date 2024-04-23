01b409899c0b5bccde26882e830e36d84063df19
Hazmed <mail@hazmedmoreno.com>
2024-04-23 00:30:07 -0400

    updated api endpoints

--- client/src/app/api-calls/index.js
+++ client/src/app/api-calls/index.js
@@ -11,7 +11,11 @@ export function submitQuoteRequest(data) {
 }
 
 export function getQuotes(email) {
-  return fetch(API_ENDPOINT + "/quote/" + email, {
-    method: "GET",
+  return fetch(API_ENDPOINT + "/quotes/all", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
   });
 }
