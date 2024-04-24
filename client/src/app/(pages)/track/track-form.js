c6358060f9c23e730deca86d598195e918335e54
Hazmed <mail@hazmedmoreno.com>
2024-04-24 17:37:13 -0400

    upd: track form responsiveness css

--- client/src/app/(pages)/track/track-form.js
+++ client/src/app/(pages)/track/track-form.js
@@ -19,17 +19,17 @@ export default function TrackForm( { fetchQuote, error }) {
   });
 
   return (
-    <section className="w-full min-h-[70vh]  flex flex-col items center justify-center relative">
+    <section className="w-full min-h-[70vh]  flex flex-col items center justify-center relative bg-black">
     <div className="absolute bg-truck h-full bg-cover bg-no-repeat blur w-full"></div>
     <div className="absolute bg-zinc-950 bg-opacity-70 h-full w-full"></div>
-    <div className="w-fit m-auto text-white bg-black">
+    <div className="w-fit m-auto text-white p-4">
       <h1 className="mb-2 relative text-white">
         <span className="text-orange-600">Find</span> and{" "}
         <span className="text-sky-600">Track</span> Your Quotes
       </h1>
     <form
       onSubmit={formik.handleSubmit}
-      className="relative z-10 text-white w-fit m-auto items-start flex flex-col"
+      className="relative z-10 text-white w-ful lg:w-fit m-auto items-start flex flex-col"
     >
       <div className="flex w-full gap-2 items-center justify-center flex-col lg:flex-row">
         <label className="w-full">
